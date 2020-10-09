d9200a17b79c1115c10f638d374855773870f3ea
ramnet04 <37320802+ramnet04@users.noreply.github.com>
2020-10-09 20:22:01 +0530

    Add files via upload

---
+++ Class1.java
@@ -0,0 +1,15 @@
+package package1;
+
+public class Class1 {
+	public static void main(String[] args) {
+		String str = "success";
+		char str1 = ' ';
+
+		for (int i = 0; i < str.length(); i++) {
+			System.out.println(str.charAt(i));
+			str1 = str.charAt(i);
+			
+			
+		}
+	}
+}
